9953e0c1480494c46b58f17d800a1f2a37b584b2
IGamesI <69199901+IGamesI@users.noreply.github.com>
2023-01-26 14:23:58 +0330

    Added teach

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ gameData = {
     "Player1": {
         "Coins": 0,
         "Coin_Generator": 20,
-        "Soliders": []
+        "Soldiers": []
     },
     "Player2": {
         "Coins": 0,
         "Coin_Generator": 20,
-        "Soliders": []
+        "Soldiers": []
     }
 }
 
@@ -16,8 +16,30 @@ gameData = {
 def get_status():
     returnData = {
         "first castle health": gameData["Player1"]["Coin_Generator"],
-        "first castle soldiers": gameData["Player1"]["Soliders"],
+        "first castle soldiers": gameData["Player1"]["Soldiers"],
         "second castle health": gameData["Player2"]["Coin_Generator"],
-        "second castle soldiers": gameData["Player2"]["Soliders"],
+        "second castle soldiers": gameData["Player2"]["Soldiers"],
     }
     return returnData
+
+
+def teach(team_id, soldier_id):
+    if team_id == "first":
+        team_id = "Player1"
+    elif team_id == "second":
+        team_id = "Player2"
+    else:
+        raise ValueError(
+            f"{team_id} is not acceptable, only 'first' and 'second' is defined")
+
+    soldier_id = eval(soldier_id)
+    if soldier_id < 2:
+        raise ValueError(
+            f"{soldier_id} is not acceptable, only numbers above 1 are acceptable")
+    else:
+        if gameData[team_id]["Coins"] < soldier_id:
+            return False
+        else:
+            gameData[team_id]["Coins"] -= soldier_id
+            gameData[team_id]["Soldiers"].append([soldier_id - 1, 0])
+            return True
